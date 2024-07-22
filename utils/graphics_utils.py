6c810bbf13e897ec5e507ffe4fd81a6be24dad0a
Interstellar6 <632476769@qq.com>
2024-07-23 02:55:44 +0800

    first commit

--- utils/graphics_utils.py
+++ utils/graphics_utils.py
@@ -35,19 +35,29 @@ def getWorld2View(R, t):
     Rt[3, 3] = 1.0
     return np.float32(Rt)
 
+# 转换世界坐标系到相机坐标系
+# 参数： 旋转 平移矩阵 要转换的参数数组 缩放因子
 def getWorld2View2(R, t, translate=np.array([.0, .0, .0]), scale=1.0):
+    # 世界坐标系到相机坐标系的转换矩阵
+    # Rt = [[ R_{3*3}, t_{3*1}]
+    #       [ 0      , 1      ]]
     Rt = np.zeros((4, 4))
     Rt[:3, :3] = R.transpose()
     Rt[:3, 3] = t
     Rt[3, 3] = 1.0
 
+    # 相机坐标系到世界坐标系到转换矩阵 Rt^{-1} 实际就是相机的内参矩阵
     C2W = np.linalg.inv(Rt)
+    # 相机坐标
     cam_center = C2W[:3, 3]
+    # 移动缩放得到新相机中心
     cam_center = (cam_center + translate) * scale
     C2W[:3, 3] = cam_center
+    # 再逆回去就是新的世界坐标系到相机坐标系转换矩阵
     Rt = np.linalg.inv(C2W)
     return np.float32(Rt)
 
+# 相机视野的最近点 最远点 水平方向与垂直放心的视野范围
 def getProjectionMatrix(znear, zfar, fovX, fovY):
     tanHalfFovY = math.tan((fovY / 2))
     tanHalfFovX = math.tan((fovX / 2))
